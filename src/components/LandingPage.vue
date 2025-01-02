bff0eee7ff6cb4d8d55283fd602cbb56017745bf
ROBBZZ <bynho92k@gmail.com>
2025-01-02 13:11:49 -0300

    create Artist section

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,16 +1,24 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive
-      class="align-centerfill-height mx-auto"
-      max-width="900"
-    >
-    <main>
-      <div id="perfil">
-        <img src="/src/assets/artist-img.png" alt="Billie Eilish">
+    <main class="d-flex">
+      <div id="perfil" class="d-flex ml-10 mt-10">
+        <img src="/src/assets/artist-img.png" alt="Billie Eilish" class="perfil">
+      </div>
+      <div class="info">
+        <h1 class="text-h3 font-weight-bold mt-11 ml-6 mb-10">Billie Eilish</h1>
+        <p class="ml-7">With "Bad Guy", she became the first <br> 
+          artist born in the 21st century and third <br>
+          Gen Z artist <a href="#">MORE</a></p>
+      </div>
+      <div class="category">
+        <h2>Dance/Pop</h2>
+        <div class="buttons-track">
+         <v-btn variant="plain"></v-btn>
+         <v-btn variant="plain"></v-btn>
+         <v-btn variant="plain"></v-btn>
+         <v-btn variant="plain"></v-btn>
+        </div>
       </div>
     </main>
-    </v-responsive>
-  </v-container>
 </template>
 
 <script setup>
@@ -19,7 +27,21 @@
 
 <style>
 
-  * {
+  main {
     background: linear-gradient(to right,#000000, #2D1C1C, #6A2525);
+    color: white;
+  }
+
+  img.perfil {
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+    color: #6687FF;
+  }
+
+  .category {
+    margin-top: 80px;
+    margin-left: 27vw;
   }
 </style>
